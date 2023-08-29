36a9da28a98fad5e8175855873a3b2f45de93526
GodPepe7 <privatalex@gmail.com>
2023-08-29 18:38:47 +0200

    Basic HTML Layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('test');
+import css from './styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index-template.html'
+    }),
+  ],
+  output: {
+    filename: 'bundle.js.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
+  },
 };
